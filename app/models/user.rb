34bdc3cdd0500b807ea6dc4def0e5b4f30de33e9
Heber Massami Nunomura <heber28@gmail.com>
2011-09-14 09:24:17 -0300

    pequentas alterações no email

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   validates :password, :on => :create, :presence => true
   #validates_presence_of :password, :on => :create
   validates_confirmation_of :password
-  validates_length_of :password, :minimum => 4, :allow_blank => true
+  validates_length_of :password, :minimum => 6, :allow_blank => true
 
   # login can be either username or email address
   def self.authenticate(login, pass)
